abfb2a02c7f4c7a20a6f33a485a1ddbdaa704250
ivana <ivanasinkin@gmail.com>
2022-02-17 15:26:14 +0300

    Second version

---
+++ main.py
@@ -0,0 +1,12 @@
+import sys
+
+from PyQt5 import QtGui, QtWidgets
+from ui_Main_form import Ui_MainWindow
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    mainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(mainWindow)
+    mainWindow.show()
+    sys.exit(app.exec())
