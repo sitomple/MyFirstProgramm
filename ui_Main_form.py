8b99c86073f85cb3e92bed4eed7db606c4d508fc
ivana <ivanasinkin@gmail.com>
2022-02-17 18:55:00 +0300

    Second version

--- ui_Main_form.py
+++ ui_Main_form.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'Main-form.ui'
+# Form implementation generated from reading ui file 'ui_Main_form.ui'
 #
 # Created by: PyQt5 UI code generator 5.15.6
 #
@@ -14,28 +14,37 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(800, 600)
+        MainWindow.resize(1098, 696)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.label = QtWidgets.QLabel(self.centralwidget)
-        self.label.setGeometry(QtCore.QRect(250, 220, 311, 101))
+        self.label.setGeometry(QtCore.QRect(380, 230, 311, 101))
         font = QtGui.QFont()
         font.setPointSize(36)
         self.label.setFont(font)
         self.label.setObjectName("label")
+        self.closeButton = QtWidgets.QPushButton(self.centralwidget)
+        self.closeButton.setGeometry(QtCore.QRect(210, 590, 621, 41))
+        self.closeButton.setObjectName("closeButton")
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 21))
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 1098, 21))
         self.menubar.setObjectName("menubar")
+        self.menu = QtWidgets.QMenu(self.menubar)
+        self.menu.setObjectName("menu")
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
         self.statusbar.setObjectName("statusbar")
         MainWindow.setStatusBar(self.statusbar)
+        self.menubar.addAction(self.menu.menuAction())
 
         self.retranslateUi(MainWindow)
+        self.closeButton.clicked.connect(MainWindow.close) # type: ignore
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "ZumaGame"))
         self.label.setText(_translate("MainWindow", "Привет Мир!!!"))
+        self.closeButton.setText(_translate("MainWindow", "Закрыть"))
+        self.menu.setTitle(_translate("MainWindow", "Настройки"))
